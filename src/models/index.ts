93671eb6a9bcb796f5bcb55f1f4c8c5d5a9ef63e
Ame <ame180x@gmail.com>
2024-11-03 04:07:20 +0100

    Add GuildConfigs to store per-guild configs in a JSON format

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,9 +1,11 @@
 import { connection, DataTypes } from '../services/DatabaseProvider.js';
 import getUserModel from './User'
 import getUserGuildModel from './UserGuild'
+import getGuildConfigModel from './GuildConfig'
 
 const UserModel = getUserModel(connection, DataTypes);
 const UserGuildModel = getUserGuildModel(connection, DataTypes);
+const GuildConfigModel = getGuildConfigModel(connection);
 
 UserModel.hasOne(UserGuildModel, { foreignKey: 'userId' });
 UserGuildModel.belongsTo(UserModel, { foreignKey: 'userId' });
@@ -11,5 +13,6 @@ UserGuildModel.belongsTo(UserModel, { foreignKey: 'userId' });
 export {
     UserModel,
     UserGuildModel,
+    GuildConfigModel,
     connection
 }
