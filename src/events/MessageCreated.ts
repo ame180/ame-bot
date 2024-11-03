65b8f714e76c5c7115c8d1844b2071e46ace0927
Ame <ame180x@gmail.com>
2024-11-03 04:50:38 +0100

    Add handleReactions to MessageCreated.ts

--- src/events/MessageCreated.ts
+++ src/events/MessageCreated.ts
@@ -1,7 +1,9 @@
 import { handle as handleXp } from '../services/MessageXpHandler.js';
+import { handle as handleReactions } from '../services/MessageReactionsHandler.js';
 import {Events} from "discord.js";
 
 export const name = Events.MessageCreate;
 export async function execute(message) {
     await handleXp(message);
+    await handleReactions(message);
 }
