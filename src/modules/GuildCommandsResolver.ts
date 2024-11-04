597c78182dc9ca8d1f555446e7371374ff1add08
Ame <ame180x@gmail.com>
2024-11-04 06:08:34 +0100

    change guildCommands const into a function to handle dynamic commands, update GuildCommandsResolver to handle it

--- src/modules/GuildCommandsResolver.ts
+++ src/modules/GuildCommandsResolver.ts
@@ -1,12 +1,13 @@
-import { guildCommands } from "./index";
+import { getGuildCommands as getModuleGuildCommands } from "./index";
 import { getEnabledGuildModules } from "./GuildModulesResolver";
 
 export async function getGuildCommands(guildId: string) {
     const modules = await getEnabledGuildModules(guildId);
+    const allModuleCommands = await getModuleGuildCommands(guildId);
     let commands = {};
     for (const module of modules)
     {
-        const moduleCommands = guildCommands[module];
+        const moduleCommands = allModuleCommands[module];
         commands = {
             ...commands,
             ...moduleCommands
--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,13 +1,17 @@
 import * as main from './main';
 import * as levels from './levels';
 import * as messageReaction from './messageReactions';
+import * as targetedGifs from './targetedGifs';
 
 export const globalCommands = {
     ...main.commands,
 }
 
-export const guildCommands = {
-    [levels.name]: levels.commands
+export async function getGuildCommands(guildId: string) {
+    return {
+        [levels.name]: levels.commands,
+        [targetedGifs.name]: await targetedGifs.getGuildCommands(guildId),
+    }
 }
 
 export const globalEventHandlers = []
