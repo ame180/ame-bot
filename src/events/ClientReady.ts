f193f954c10da4eaadafa4a25b1789a59b881ee5
Ame <ame180x@gmail.com>
2024-11-10 01:09:02 +0100

    Update CommandRegisterer to take an array of guild instead of just IDs, update ClientReady event to map simple Guild objects rather than IDs and call GuildUpdater as well

--- src/events/ClientReady.ts
+++ src/events/ClientReady.ts
@@ -1,11 +1,18 @@
 import { Events } from "discord.js";
 import { registerCommands } from "../services/CommandRegisterer";
+import {updateGuilds} from "../services/GuildUpdater";
 
 export const name = Events.ClientReady;
 export const once = true;
 export async function execute(client) {
     console.log(`Ready! Logged in as ${client.user.tag}`);
 
-    const guildIds = client.guilds.cache.map(guild => guild.id);
-    await registerCommands(guildIds);
+    const guilds = client.guilds.cache.map((guild) => {
+        return {
+            id: guild.id,
+            name: guild.name
+        }
+    });
+    registerCommands(guilds).then();
+    updateGuilds(guilds).then();
 }
